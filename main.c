5fc87c7b13eb141b292ce2f8b69abb1c45d7ebc3
Javier Luna <javierlunamolina@gmail.com>
2014-11-05 14:01:49 +0100

    Revert "Revert "connect4""
    This reverts commit adabb1a6d6070e3835f0fd8aa35a01a7393ee847.

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<conecta4.h>
+
+void main(){
+char jugar='y';
+do{
+    reset();
+    a_jugar();
+    printf("¿Otra partida?(y/n)\n:");
+    fflush(stdin);
+    scanf("%c",&jugar);
+}while(jugar=='y' || jugar=='Y');
+}
+
+void a_jugar(){
+ int lector;
+
+    pintar();
+while(juego){
+        printf("Introduce la columna donde quiera poner su ficha (fichas restantes %d):", n_fichas);
+    scanf("%d",&lector);
+    while(!meter_ficha(1,lector)){
+        scanf("%d", &lector);
+    }
+    //meter_ficha(2,lector);
+     if(comprobar(1)){
+
+        juego=0;
+     }
+        pintar();
+
+}
+        printf("HA GANADO MAMONA!");
+        reset();
+}
